840f6c332c9a36d4ffb961afd4fabd3c8d7b950c
A Thomas Bianchi <atbianchi@gmail.com>
2020-04-26 21:41:05 -0400

    1 solved

---
+++ 1.js
@@ -0,0 +1,17 @@
+
+// find all multiples of 3 or 5
+// less than the number given
+// sum those numbers
+
+function findSum(num) {
+  const multiples = [];
+  for (let i = 0; i < num; i++) {
+    if (
+      i % 3 === 0 ||
+      i % 5 === 0
+    ) {
+      multiples.push(i);
+    }
+  }
+  return multiples.reduce((a, b) => a + b)
+}
